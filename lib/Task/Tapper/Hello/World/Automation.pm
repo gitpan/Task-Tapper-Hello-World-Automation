351db8c6fc6892c6fabcdf06ddc2490b8fa2dedd
Tapper Team <tapper@tapper-testing.org>
2012-11-26 09:03:06 +0000

    Import of TAPPER/Task-Tapper-Hello-World-Automation-0.001 from CPAN.
    gitpan-cpan-distribution: Task-Tapper-Hello-World-Automation
    gitpan-cpan-version:      0.001
    gitpan-cpan-path:         TAPPER/Task-Tapper-Hello-World-Automation-0.001.tar.gz
    gitpan-cpan-author:       TAPPER
    gitpan-cpan-maturity:     released

---
+++ lib/Task/Tapper/Hello/World/Automation.pm
@@ -0,0 +1,50 @@
+use strict;
+use warnings;
+package Task::Tapper::Hello::World::Automation;
+# git description: 9a7f21c
+
+BEGIN {
+  $Task::Tapper::Hello::World::Automation::AUTHORITY = 'cpan:TAPPER';
+}
+{
+  $Task::Tapper::Hello::World::Automation::VERSION = '0.001';
+}
+# ABSTRACT: Tapper - The easiest start for automation without hassle (hopefully)
+
+__END__
+
+=pod
+
+=head1 NAME
+
+Task::Tapper::Hello::World::Automation - Tapper - The easiest start for automation without hassle (hopefully)
+
+=head1 VERSION
+
+version 0.001
+
+=head1 TASK CONTENTS
+
+=head2 Tapper
+
+=head3 L<Task::Tapper::Hello::World>
+
+=head3 L<Tapper::MCP>
+
+=head3 L<Tapper::MCP::MessageReceiver>
+
+1;
+
+=head1 AUTHOR
+
+Steffen Schwigon <ss5@renormalist.net>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is Copyright (c) 2012 by Steffen Schwigon.
+
+This is free software, licensed under:
+
+  The (two-clause) FreeBSD License
+
+=cut
